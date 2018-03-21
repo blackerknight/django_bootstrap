125cba259b684995e7906e81b2c7f9dc852ff28a
dell <you@example.com>
2018-03-21 14:08:45 -0600

    se agregó la libreria de bootstrap con django y se pusieron los archivo en static y templates

--- aplicaciones/paginas/urls.py
+++ aplicaciones/paginas/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from aplicaciones.paginas import views
 
 urlpatterns = [
-    url(r'^pagina1/$', views.HomePageView.as_view(), name='pagina1')
+    url(r'^home/$', views.HomePageView.as_view(), name='home'),
+    url(r'^password_change/$', views.HomePageView.as_view(), name='password_change'),
+    url(r'^logout/$', views.HomePageView.as_view(), name='logout'),
+    url(r'^login/$', views.HomePageView.as_view(), name='login'),
+
 ]
